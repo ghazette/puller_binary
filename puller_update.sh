61a0839d576f5add1a91dffde636bfd565d7e195
ghazette <hazette.gregoire@gmail.com>
2018-03-30 15:09:35 +0200

    #5

--- puller_update.sh
+++ puller_update.sh
@@ -6,7 +6,7 @@ LGREEN=$(echo '\033[01;32m')
 
 #wait puller process to close
 sleep 1
-echo ${GREEN}"Puller debut de la mise à jour\n"${RESTORE}
+echo ${GREEN}"Puller début de la mise à jour"${RESTORE}
 
 #Check if git is installed
 git --version >/dev/null 2>&1 || { echo ${LRED}Arrêt de l\'installation :${RESTORE} git n\'est pas installé.; exit 1; }
@@ -28,4 +28,4 @@ rm "$HOME/.puller/puller"
 mv "$HOME/.puller_binary/puller" "$HOME/.puller/puller"
 rm -rf "$HOME/.puller_binary"
 
-echo ${GREEN}"\nMise à jour du client terminé.\n"${RESTORE};
+echo ${GREEN}"\nMise à jour du client terminé."${RESTORE};
